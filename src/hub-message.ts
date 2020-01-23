398f2e9fe52f9d484240806bd51a155aae40979e
Lorenzo Colautti <colautti.lorenzo@gmail.com>
2020-01-23 09:50:40 +0100

    initial commit

--- src/hub-message.ts
+++ src/hub-message.ts
@@ -9,22 +9,23 @@
     Version             1.0.0
     Licence             MIT Licence
     +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    
+
     -- chanels creation
-    HubMessage.channels.create('test');             // crea il canale 'test'
+    HubMessage.channels.create('test');:            // crea il canale 'test'
     HubMessage.channels.create('test02');           // crea il canale 'test02'
     HubMessage.channels.create('test03');           // crea il canale 'test03'
 
     -- obtaining all registered chanels
-    HubMessage.channels.get();
+    HubMessage.channels.get('test02');               // estrae il canale 'test02'
+    HubMessage.use('test02');
 
     -- using default chanel
-    HubMessage.subscribe('message01', function (param) { });
-    HubMessage.publish('message01');
-    HubMessage.unsubscribe('message01');
+    HubMessage.use('test02').subscribe('message01', function (param) { });
+    HubMessage.use('test02').publish('message01', {});
+    HubMessage.use('test02').unsubscribe('message01');
 
     -- broadcast on every registered chanel (also default chanel)
-    HubMessage.broadcast('message01');
+    HubMessage.broadcast('message01', {});
 
     -- using a specific registered chanel
     HubMessage.use('test').subscribe('message-test', function (param) { });
@@ -32,176 +33,183 @@
     HubMessage.use('test').unsubscribe('message-test');
 
     -- removing chanel named: 'test03'
-    HubMessage.use('test03').dispose();
+    HubMessage.channels.remove('test03');
 
     -- cloning the chanel named 'test02' in a new chanel named 'test03'
-    HubMessage.use('test').clone('test03');
+    var channel = HubMessage.use('test02');
+    HubMessage.channels.clone('test03', channel);
 
 */
 
+class Message {
 
-export class HubMessage {
+    public readonly name: string;
+    public readonly delegate: Function;
+
+    public constructor(name: string, delegate: Function) {
+        this.name = name;
+        this.delegate = delegate;
+    }
 
 }
 
-declare const window: any;
+class Channel {
+
+    public readonly isChannel: boolean = true;
+
+    public readonly messages: Message[] = [];
+    public readonly name: string;
+
+    public readonly instance: Channel;
+
+    public constructor(name: string) {
+        this.name = name;
+        this.instance = this;
+    }
+
+    private getMessageByName(messageName: string): Message|undefined {
+        if(messageName == undefined || messageName == '') {
+            throw 'messageName must be valued';
+        }
+
+        let message: Message|undefined = undefined;
+        for(let i = 0; i < this.messages.length; i++) {
+            if(this.messages[i].name == messageName) {
+                message = this.messages[i];
+                break;
+            }
+        }
+
+        return message;
+    }
+
+    public subscribe(messageName: string, delegate: Function): void {
+        if(messageName == undefined || messageName == '') {
+            throw 'messageName must be valued';
+        }
+
+        if(delegate == undefined || (delegate != undefined && typeof delegate == 'function')) {
+            throw 'delegate must be type of Function';
+        }
+
+        const message: Message = new Message(messageName, delegate)
+        this.messages.push(message);
+    }
+
+    public unsubscribe(messageName: string): void {
+        if(messageName == undefined || messageName == '') {
+            throw 'messageName must be valued';
+        }
+
+        let messageIndex: number = -1;
+        for(let i = 0; i < this.messages.length; i++) {
+            if(this.messages[i].name == messageName) {
+                messageIndex = i;
+                break;
+            }
+        }
+
+        if(messageIndex > -1) {
+            this.messages.splice(messageIndex, 1);
+        }
+    }
+
+    public publish(messageName: string, data: any): void {
+        if(messageName == undefined || messageName == '') {
+            throw 'messageName must be valued';
+        }
+
+        const message: Message|undefined = this.getMessageByName(messageName);
+        if(message == undefined) {
+            throw 'message not found in channel ' + this.instance.name;
+        }
+
+        const param: any = { event: { channel: this.instance, message: message }, data: data };
+        message.delegate(param);
+    }
+
+}
+
+class ChannelCollection {
+
+    public readonly channels: Channel[];
+
+    public constructor() {
+        this.channels = [];
+    }
+
+    public create(channelName: string): void {
+        if(channelName == undefined || channelName == '') {
+            throw 'channelName must be valued';
+        }
+
+        const channel: Channel = new Channel(channelName);
+        this.channels.push(channel);
+    }
+
+    public get(channelName: string): Channel|undefined {
+        if(channelName == undefined || channelName == '') {
+            throw 'channelName must be valued';
+        }
+
+        let channel: Channel|undefined = undefined;
+        for(let i = 0; i < this.channels.length; i++) {
+            if(this.channels[i].name == channelName) {
+                channel = this.channels[i];
+                break;
+            }
+        }
+
+        return channel;
+    }
+
+    public remove(channelName: string) {
+        if(channelName == undefined || channelName == '') {
+            throw 'channelName must be valued';
+        }
+    }
+
+    public clone(channelName: string, channel: Channel) {
+        if(channelName == undefined || channelName == '') {
+            throw 'channelName must be valued';
+        }
+
+        if(channel == undefined || (channel != undefined && channel.isChannel == true)) {
+            throw 'channel must be type of Channel';
+        }
+        
+    }
+
+}
+
+export abstract class HubMessage {
+
+    public static readonly channels: ChannelCollection = new ChannelCollection();
+
+    public static use(channelName: string): Channel|undefined {
+        if(channelName == undefined || channelName == '') {
+            throw 'channelName must be valued';
+        }
+
+        return HubMessage.channels.get(channelName);
+    }
+
+    public static broadcast(messageName: string, data: any): void {
+        if(messageName == undefined || messageName == '') {
+            throw 'messageName must be valued';
+        }
+
+        for(let i = 0; i < HubMessage.channels.channels.length; i++) {
+            HubMessage.channels.channels[i].publish(messageName, data);
+        }
+    }
+
+}
+
+declare global {
+    interface Window {
+        HubMessage: HubMessage;
+    }
+}
+
 window.HubMessage = HubMessage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(function () {
-
-    window.message = new function () {
-
-        var _DEFAULT_KEY = '_';
-        var _hub = {};
-
-        _hub[_DEFAULT_KEY] = {};
-
-        var _message = function (channel) {
-
-            if (channel == undefined || (channel != undefined && channel.replace(/ /gm) == '')) {
-                channel = _DEFAULT_KEY;
-            }
-
-            this.instance = function (channel) {
-
-                var channel = channel;
-                var createParameter = function (channel, message, data) {
-                    return {
-                        event: { channel: channel, message: message },
-                        data: data
-                    };
-                };
-
-                this.publish = function (message, data) {
-                    //TODO: check if _hub[channel] exists
-                    //TODO: check if _hub[channel][message] exists
-                    var param = createParameter(channel, message, data);
-                    for (var i = 0; i < _hub[channel][message].length; i++) {
-                        _hub[channel][message][i](param);
-                    }
-
-                    delete param;
-                };
-
-                if (channel == _DEFAULT_KEY) {
-                    this.broadcast = function (message, data) {
-                        //TODO: check if _hub[channel] exists
-                        //TODO: check if _hub[channel][message] exists
-                        for (var __key in _hub) {
-                            if (_hub[__key][message] != undefined) {
-                                var param = createParameter(__key, message, data);
-                                for (var i = 0; i < _hub[__key][message].length; i++) {
-                                    _hub[__key][message][i](param);
-                                }
-                            }
-                        }
-
-                        delete param;
-                    };
-                }
-
-                this.subscribe = function (message, handler) {
-                    //TODO: check if _hub[channel] exists
-                    //TODO: check if _hub[channel][message] exists
-                    if (_hub[channel][message] == undefined) {
-                        _hub[channel][message] = [];
-                    }
-
-                    _hub[channel][message].push(handler);
-                };
-
-                this.unsubscribe = function (message) {
-                    //TODO: check if _hub[channel] exists
-                    //TODO: check if _hub[channel][message] exists
-                    delete _hub[channel][message];
-                };
-
-                this.clone = function (newName) {
-                    //TODO: check if _hub[channel] exists
-                    if (_hub[newName] == undefined) {
-                        _hub[newName] = Object.assign({}, _hub[channel]);
-                    }
-                    else {
-                        //TODO: warning ...key already exists
-                    }
-                };
-
-                this.dispose = function () {
-                    if (channel == undefined || (channel != undefined && channel.replace(/ /gm) == '')) {
-                        //TODO: warning ...channel not defined or empty
-                        return;
-                    }
-
-                    if (channel == _DEFAULT_KEY) {
-                        //TODO: warning ...channel could not be default
-                        return;
-                    }
-
-                    if (_hub[channel] != undefined) {
-                        _hub[channel] = undefined;
-                        delete _hub[channel];
-                    }
-                    else {
-                        //TODO: warning ...key not found
-                    }
-                };
-
-            };
-
-            return new instance(channel);
-
-        };
-
-        var me = this;
-
-        me.channels = {
-
-            create: function (name) {
-                if (_hub[name] == undefined) {
-                    _hub[name] = Object.assign({}, _hub[_DEFAULT_KEY]);
-                }
-                else {
-                    //TODO: warning ...key already exists
-                }
-            },
-
-            get: function () {
-                return Object.keys(_hub);
-            }
-
-        };
-
-        me.message = _message(_DEFAULT_KEY);
-
-        me.useChanel = function (name) {
-            return _message(name);
-        };
-
-        return {
-            channels: me.channels,
-            use: me.useChanel,
-            subscribe: me.message.subscribe,
-            unsubscribe: me.message.unsubscribe,
-            publish: me.message.publish,
-            broadcast: me.message.broadcast
-        };
-
-    };
-
-})();
